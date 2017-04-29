d72e14a487080dc061a4c363f8f0bb079d60dd90
kingEND <2578038306@qq.com>
2017-04-30 02:39:15 +0800

    添加index.js和index.css

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+let MenuNode = document.getElementsByClassName('menu');
+let itemNode = MenuNode[0].children;
+
+// id 测试
+let Menu = document.getElementById('test');
+let item = Menu.getElementsByClassName('item');
+
+console.log(MenuNode);
+console.log(itemNode);
+console.log(Menu);
+console.log(item);
+// console.log(itemNode);
